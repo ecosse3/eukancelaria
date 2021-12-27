f32b9c07b0813287292e7f84160f848e588b8ae4
ecosse3 <luk.kurpiewski@gmail.com>
2021-12-27 19:18:03 +0100

    feat: add fontawesome icons to _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,9 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faEnvelope, faHome, faPhone } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faHome, faPhone, faEnvelope);
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
