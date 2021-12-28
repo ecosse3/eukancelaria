7c750e97d4f611e220e640fd715d2f7c1cba5b98
ecosse3 <luk.kurpiewski@gmail.com>
2021-12-29 00:59:33 +0100

    feat: add about section to index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import Image from 'next/image';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import Logo from '../assets/images/logo.png';
 
 const Home: NextPage = () => {
   return (
@@ -17,18 +19,35 @@ const Home: NextPage = () => {
       </Head>
       <Header />
       <main>
+        {/* Hero */}
         <div className="relative w-full h-screen bg-no-repeat bg-cover bg-[position:80%] bg-[url('../assets/images/bg_mobile.jpg')] md:bg-[url('../assets/images/bg.jpg')]">
           <div className="flex max-w-7xl mx-auto w-full h-full place-items-center">
-            <div className="font-montserrat animate mx-3 max-w-[10rem] lg:max-w-lg">
-              <h1 className="leading-8 italic text-2xl lg:text-3xl">
+            <div className="animate mx-4 max-w-[10rem] lg:max-w-lg">
+              <h1 className="font-montserrat text-primary leading-7 font-semibold italic text-2xl lg:leading-10 lg:text-3xl">
                 Scire leges non hoc est verba earum tenere, sed vim ac potestatem
               </h1>
-              <h2 className="font-light leading-7 text-lg lg:text-xl">
+              <h2 className="font-roboto text-slate-600 mt-2 font-light leading-6 text-lg lg:leading-7 lg:text-xl">
                 - Znajomość praw polega nie na pamiętaniu ich dosłownie, ale na
                 umiejętności stosowania ich sensu
               </h2>
             </div>
           </div>
+        </div>
+        {/* About */}
+        <div className="flex flex-col max-w-7xl mx-auto py-16 items-center">
+          <Image
+            src={Logo}
+            alt="Ewa Urbanowicz - Kancelaria Radcy Prawnego"
+            width={250}
+            height={104}
+          />
+          <span className="text-md text-slate-500 mx-4 my-16 text-center max-w-3xl">
+            Kancelaria oferuje kompleksową obsługę prawną przedsiębiorców, jak i świadczy
+            usługi prawne dla osób fizycznych. Poza nieustannym dążeniem do doskonałości
+            zawodowej, przy jednoczesnym poszanowaniu zasad etyki, pamiętamy, że każdy
+            Klient i każda sprawa wymaga szczególnego, zindywidualizowanego podejścia.
+          </span>
+          <button className="btn">Dowiedz się więcej</button>
         </div>
       </main>
     </React.Fragment>
