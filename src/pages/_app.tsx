08fd8c4f50e07046abfe781ea75705030b4a2845
ecosse3 <luk.kurpiewski@gmail.com>
2021-12-29 00:08:59 +0100

    feat: add brand facebook icon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,9 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faEnvelope, faHome, faPhone } from '@fortawesome/free-solid-svg-icons';
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
 
-library.add(faHome, faPhone, faEnvelope);
+library.add(faHome, faPhone, faEnvelope, faFacebook);
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
