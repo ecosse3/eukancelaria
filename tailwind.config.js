feaa4ccfda618a09b643000f1deffc8f10caf2af
ecosse3 <luk.kurpiewski@gmail.com>
2021-12-29 00:09:32 +0100

    feat: extend tailwind with colors & fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 module.exports = {
   content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#2C2947',
+        secondary: '#CB85AF'
+      }
+    },
     fontFamily: {
-      roboto: ['Roboto', 'sans-serif']
+      roboto: ['Roboto', 'sans-serif'],
+      rubik: ['Rubik', 'sans-serif'],
+      montserrat: ['Montserrat', 'sans-serif']
     }
   },
   plugins: []
