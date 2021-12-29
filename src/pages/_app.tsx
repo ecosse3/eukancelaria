6c18e4095ce1e1a38eb03bf04bf46f2fe8ca9f23
ecosse3 <luk.kurpiewski@gmail.com>
2021-12-29 22:36:08 +0100

    chore: use GlobalStyles in _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,19 @@
-import '../styles/globals.css';
+import '../styles/tailwind.css';
 import type { AppProps } from 'next/app';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faEnvelope, faHome, faPhone } from '@fortawesome/free-solid-svg-icons';
 import { faFacebook } from '@fortawesome/free-brands-svg-icons';
+import GlobalStyles from '../styles/GlobalStyles';
 
 library.add(faHome, faPhone, faEnvelope, faFacebook);
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
 export default MyApp;
