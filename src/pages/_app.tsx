a814c97d97e13b17933f75f8ac576bc6fbca3a85
ecosse3 <luk.kurpiewski@gmail.com>
2021-12-30 23:55:50 +0100

    feat: add LinkedIn & Twitter icons

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,10 @@ import '../styles/tailwind.css';
 import type { AppProps } from 'next/app';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faEnvelope, faHome, faPhone } from '@fortawesome/free-solid-svg-icons';
-import { faFacebook } from '@fortawesome/free-brands-svg-icons';
+import { faFacebook, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons';
 import GlobalStyles from '../styles/GlobalStyles';
 
-library.add(faHome, faPhone, faEnvelope, faFacebook);
+library.add(faHome, faPhone, faEnvelope, faFacebook, faTwitter, faLinkedin);
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
