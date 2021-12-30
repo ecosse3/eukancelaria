7fad9d73b98f68ab43fddd06f389afd97fbe759f
ecosse3 <luk.kurpiewski@gmail.com>
2021-12-30 23:56:26 +0100

    feat: support extra classnames in Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,22 +1,43 @@
-import tw, { styled } from 'twin.macro';
+import styled from 'styled-components';
+import tw from 'twin.macro';
 
 // Interfaces
 interface IProps {
   background: 'primary' | 'secondary';
   children: React.ReactText;
+  className?: string & React.CSSProperties;
   onClick?: () => void;
 }
 
 // Component
 
 const StdButton = styled.button(({ background }: IProps) => [
-  tw`text-xs py-4 px-6 font-normal tracking-wide rounded-[100px] shadow-xl uppercase outline-none text-white transition-shadow hover:transition-shadow ease-in-out hover:shadow-xl`,
+  tw`
+  text-xs
+  text-white
+  font-normal
+  uppercase
+  tracking-wide
+  py-4
+  px-6
+  rounded-[100px]
+  shadow-xl
+  outline-none
+  transition-all
+  ease-in-out
+  hover:transition-all
+  hover:shadow-xl`,
   background === 'primary' && tw`bg-primary`,
-  background === 'secondary' && tw`bg-secondary`
+  background === 'secondary' && tw`bg-secondary-400 hover:bg-secondary-500`
 ]);
 
-const Button = ({ background, children, onClick }: IProps) => (
-  <StdButton type="button" background={background} onClick={onClick}>
+const Button = ({ background, className, children, onClick }: IProps) => (
+  <StdButton
+    background={background}
+    className={className}
+    onClick={onClick}
+    type="button"
+  >
     {children}
   </StdButton>
 );
