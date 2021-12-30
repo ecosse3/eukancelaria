b270e598f9626f89dc765b3221f66e795ba85dd9
ecosse3 <luk.kurpiewski@gmail.com>
2021-12-30 23:50:32 +0100

    feat: add brand colors to tailwind config & tones to secondary

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,18 @@ module.exports = {
     extend: {
       colors: {
         primary: '#2C2947',
-        secondary: '#CB85AF'
+        secondary: {
+          100: '#E2BBD2',
+          200: '#DAA9C6',
+          300: '#D397BB',
+          400: '#CB85AF', // Main
+          500: '#C373A3',
+          600: '#BC6198',
+          700: '#B44F8C'
+        },
+        facebook: '#3B5998',
+        twitter: '#00ACEE',
+        linkedin: '#0E76A8'
       }
     },
     fontFamily: {
