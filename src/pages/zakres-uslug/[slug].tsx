6c68760985a932457f84033bf92193baddef5e8d
ecosse3 <luk.kurpiewski@gmail.com>
2022-01-03 15:46:32 +0100

    feat: add serviceHero to service page

--- src/pages/zakres-uslug/[slug].tsx
+++ src/pages/zakres-uslug/[slug].tsx
@@ -1,8 +1,11 @@
 import type { GetStaticPaths, GetStaticProps } from 'next';
 import Head from 'next/head';
+import Link from 'next/link';
 import React from 'react';
+import Button from '../../components/Button';
 import Footer from '../../components/Footer';
 import Header from '../../components/Header';
+import ServiceHero from '../../components/ServiceHero';
 import { servicesWithoutIcon } from '../../data/services';
 import { IService } from '../../types/service';
 
@@ -22,9 +25,31 @@ const Service = ({ service }: IProps) => {
         />
       </Head>
       <Header />
-      <main>
-        <div>
-          Page: {service.name} - {service.id}
+      <main className="mb-16">
+        <ServiceHero
+          color={service.textColor}
+          background={service.backgroundColor}
+          image={service.imagePath}
+          title={service.name}
+        />
+        {service.items && service.items.length > 0 ? (
+          <div className="relative max-w-7xl mx-auto text-center">
+            <h2 className="text-3xl text-gray-800">W zakres naszych usług wchodzi:</h2>
+            <ul className="max-w-2xl text-gray-500 inline-block list-disc text-left pl-4 list-outside mt-8 mx-6">
+              {service.items?.map((c) => (
+                <li key={c} className="text-lg mt-2">
+                  {c}
+                </li>
+              ))}
+            </ul>
+          </div>
+        ) : null}
+        <div className="relative max-w-7xl mx-auto text-center mt-8">
+          <Link href="/#services">
+            <a>
+              <Button background="secondary">Zobacz inne usługi</Button>
+            </a>
+          </Link>
         </div>
       </main>
       <Footer />
