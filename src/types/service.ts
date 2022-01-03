86d60a7f82476fff76310f890a0b6fc6d9ca5abe
ecosse3 <luk.kurpiewski@gmail.com>
2022-01-03 19:00:02 +0100

    feat: add description property to IService interface

--- src/types/service.ts
+++ src/types/service.ts
@@ -4,6 +4,7 @@ import { TSize } from '../components/ServiceCard';
 export interface IService {
   id: number;
   name: string;
+  description: string;
   slug: string;
   href: string;
   items?: string[];
