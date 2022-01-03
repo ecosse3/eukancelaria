80854f0d6dc599923ab559113a80c0c93c024b56
ecosse3 <luk.kurpiewski@gmail.com>
2022-01-03 16:25:12 +0100

    feat: check if links are provided then render

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -188,24 +188,26 @@ const Header = ({ accent = 'black' }: IProps) => {
                         </div>
 
                         <div className="px-5 py-5 bg-pink-50 space-y-6 sm:flex sm:space-y-0 sm:space-x-10 sm:px-8">
-                          {callsToAction.map((item) => (
-                            <div key={item.name} className="flow-root">
-                              <a
-                                href={item.href}
-                                className="-m-3 p-3 flex items-center rounded-md text-base font-medium text-gray-900 hover:bg-white"
-                              >
-                                <item.icon
-                                  icon={item.iconName}
-                                  className={classNames(
-                                    'flex-shrink-0 h-6 w-6',
-                                    item.color
-                                  )}
-                                  aria-hidden="true"
-                                />
-                                <span className="ml-3">{item.name}</span>
-                              </a>
-                            </div>
-                          ))}
+                          {callsToAction.map((item) =>
+                            item.href && item.href.length > 0 ? (
+                              <div key={item.name} className="flow-root">
+                                <a
+                                  href={item.href}
+                                  className="-m-3 p-3 flex items-center rounded-md text-base font-medium text-gray-900 hover:bg-white"
+                                >
+                                  <item.icon
+                                    icon={item.iconName}
+                                    className={classNames(
+                                      'flex-shrink-0 h-6 w-6',
+                                      item.color
+                                    )}
+                                    aria-hidden="true"
+                                  />
+                                  <span className="ml-3">{item.name}</span>
+                                </a>
+                              </div>
+                            ) : null
+                          )}
                         </div>
                         <div className="px-5 py-5 bg-gray-50 sm:px-8 sm:py-8">
                           <div>
