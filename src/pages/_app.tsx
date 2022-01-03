2fc79b7f00747a436d350a7d9f91b99b6a0d0e77
ecosse3 <luk.kurpiewski@gmail.com>
2022-01-03 15:44:56 +0100

    feat: add bubbles.css & smile icon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import '../styles/tailwind.css';
+import '../styles/bubbles.css';
 import type { AppProps } from 'next/app';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faEnvelope, faHome, faPhone } from '@fortawesome/free-solid-svg-icons';
+import { faSmile } from '@fortawesome/free-regular-svg-icons';
 import { faFacebook, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons';
 import GlobalStyles from '../styles/GlobalStyles';
 
-library.add(faHome, faPhone, faEnvelope, faFacebook, faTwitter, faLinkedin);
+library.add(faHome, faPhone, faEnvelope, faFacebook, faTwitter, faLinkedin, faSmile);
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
