862f944020e9d852cf2c264a93e7bc2707ce9545
ecosse3 <luk.kurpiewski@gmail.com>
2022-01-05 01:19:32 +0100

    feat: change hero position of about & team pages

--- src/pages/o-nas.tsx
+++ src/pages/o-nas.tsx
@@ -36,9 +36,11 @@ const About: NextPage = () => {
           md:-rotate-[18deg] md:-mt-72 md:mb-60
           xl:-rotate-[18deg] xl:-mt-[34rem]
           after:absolute after:block after:content=[''] after:z-20 after:bg-[url(/assets/about/1x/Zasob3.png)] after:bg-no-repeat
-          after:left-0 after:right-0 after:top-60 after:scale-75 after:w-full after:h-full after:bg-contain after:max-w-[440px] after:mx-auto 
-          md:after:mx-0 md:after:right-auto md:after:left-[0%] md:after:top-[60%] md:after:max-w-[750px] md:after:rotate-[18deg]
-          lg:after:left-[45%] lg:after:top-[60%] lg:after:scale-100 lg:after:rotate-[18deg]"
+          after:left-0 after:right-0 after:top-60 after:scale-75 after:w-full after:h-full after:bg-contain after:mx-auto 
+          md:after:mx-0 md:after:right-auto md:after:left-[15%] md:after:top-[30%] md:after:scale-50 md:after:rotate-[18deg]
+          lg:after:left-[15%] lg:after:top-[35%] lg:after:scale-50
+          xl:after:left-[22%] xl:after:top-[60%] xl:after:scale-[.55]
+          2xl:after:left-[22%] 2xl:after:top-[38rem] 2xl:after:scale-[.55]"
         >
           <div className="flex flex-col max-w-7xl mx-4 lg:mx-auto w-full h-full rotate-0 text-center md:text-left md:rotate-[18deg] md:pt-72 xl:pt-[34rem]">
             <h1 className="text-white text-5xl md:text-6xl mt-[20vh] md:mt-[25vh] mx-4">
--- src/pages/zespol.tsx
+++ src/pages/zespol.tsx
@@ -29,16 +29,18 @@ const About: NextPage = () => {
       </Head>
       <Header accent="white" />
       <main className="overflow-hidden">
+        {/* Hero */}
         <div
           className="relative w-full min-h-full h-[85vh] md:h-screen
           bg-gradient-to-br from-[#AF65D7] to-[#2C2947] rounded-b-[5rem] 
           md:-rotate-[18deg] md:-mt-72 md:mb-60
           xl:-rotate-[18deg] xl:-mt-[34rem]
           after:absolute after:block after:content=[''] after:z-20 after:bg-[url(/assets/team/1.5x/Zasob1@1.5x.png)] after:bg-no-repeat
-          after:left-0 after:right-0 after:top-60 after:scale-75 after:w-full after:h-full after:bg-contain after:max-w-[430px] 
-          after:mx-auto md:after:mx-0 md:after:right-auto
-          md:after:left-[0%] md:after:top-[70%] md:after:max-w-[600px] md:after:rotate-[18deg]
-          lg:after:left-[45%] lg:after:top-[70%] lg:after:scale-100 lg:after:rotate-[18deg]"
+          after:left-0 after:right-0 after:top-60 after:scale-75 after:w-full after:h-full after:bg-contain after:mx-auto md:after:mx-0 md:after:right-auto
+          md:after:left-[0%] md:after:top-[45%] md:after:scale-[.6] md:after:rotate-[18deg]
+          lg:after:left-[5%] lg:after:top-[40%] lg:after:scale-[.6] lg:after:rotate-[18deg]
+          xl:after:left-[20%] xl:after:top-[60%] xl:after:scale-50 xl:after:rotate-[18deg]
+          2xl:after:left-[20%] 2xl:after:top-[40rem] 2xl:after:scale-50 2xl:after:rotate-[18deg]"
         >
           <div className="flex flex-col max-w-7xl mx-4 lg:mx-auto w-full h-full rotate-0 text-center md:text-left md:rotate-[18deg] md:pt-72 xl:pt-[34rem]">
             <h1 className="text-white text-5xl md:text-6xl mt-[20vh] md:mt-[25vh] mx-4">
@@ -46,7 +48,8 @@ const About: NextPage = () => {
             </h1>
           </div>
         </div>
-        <section className="flex flex-col max-w-7xl mx-auto pb-16 pt-28 items-center">
+        {/* Ewa */}
+        <section className="flex flex-col max-w-7xl mx-auto pb-8 pt-28 items-center">
           <h2 className="text-slate-600 text-2xl font-light font-roboto">Radca Prawny</h2>
           <h1 className="text-slate-600 text-3xl font-bold mt-1">Ewa Urbanowicz</h1>
           <hr className="h-0.5 w-14 my-8 bg-black" />
@@ -64,7 +67,7 @@ const About: NextPage = () => {
           </p>
 
           <p className="text-xl text-slate-500 mx-4 md:mx-0 text-justify max-w-3xl mt-4">
-            Odbyła szereg kursów i szkoleń w szczególności z zakresu ochrony danych
+            Odbyła szereg kursów i szkoleń, w szczególności z zakresu ochrony danych
             osobowych. Posiada certyfikat European Network Security Institute w zakresie
             przepisów o ochronie danych osobowych. Członek rzeczywisty Stowarzyszenia
             Inspektorów Danych Osobowych.
@@ -75,6 +78,33 @@ const About: NextPage = () => {
             prowadzonej przy Okręgowej Izbie Radców Prawnych w Lublinie. Prywatnie
             pasjonuje się tańcem i jazdą na motocyklu, a w domu ma dwa koty{' '}
             <FontAwesomeIcon icon={['far', 'smile']} aria-hidden="true" />
+          </p>
+        </section>
+        {/* Tomasz */}
+        <section className="flex flex-col max-w-7xl mx-auto pb-16 pt-16 items-center">
+          <h2 className="text-slate-600 text-2xl font-light font-roboto">Radca Prawny</h2>
+          <h1 className="text-slate-600 text-3xl font-bold mt-1">Tomasz Jakubiak</h1>
+          <hr className="h-0.5 w-14 my-8 bg-black" />
+          <p className="text-xl text-slate-500 mx-4 md:mx-0 text-justify max-w-3xl">
+            Absolwent Wydziału Prawa i Administracji Uniwersytetu Marii-Curie Skłodowskiej
+            w Lublinie. Szkolił się w Krajowej Szkole Sądownictwa i Prokuratury.
+          </p>
+
+          <p className="text-xl text-slate-500 mx-4 md:mx-0 text-justify max-w-3xl mt-4">
+            Specjalista w zakresie prawa cywilnego, prawa rodzinnego i opiekuńczego, prawa
+            pracy oraz postępowań dyscyplinarnych.
+          </p>
+
+          <p className="text-xl text-slate-500 mx-4 md:mx-0 text-justify max-w-3xl mt-4">
+            Na co dzień w Kancelarii odpowiedzialny jest za reprezentowanie Klientów przed
+            sądami powszechnymi, obsługę prawną podmiotów gospodarczych i jednostek
+            sektora publicznego, a także za reprezentowanie obwinionych czy pokrzywdzonych
+            przed organami dyscyplinarnymi samorządów zawodowych, na każdym etapie
+            postępowania.
+          </p>
+
+          <p className="text-xl text-slate-500 mx-4 md:mx-0 max-w-3xl mt-4">
+            Prywatnie jego pasją jest motoryzacja oraz sporty siłowe.
           </p>
         </section>
       </main>
