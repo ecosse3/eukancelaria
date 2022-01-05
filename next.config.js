4042d35378b483fada16c238d03b4d358db31ce1
ecosse3 <luk.kurpiewski@gmail.com>
2022-01-05 01:49:29 +0100

    chore: update images domains

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['scontent-waw1-1.xx.fbcdn.net']
+    domains: ['scontent-waw1-1.xx.fbcdn.net', 'scontent-ort2-2.xx.fbcdn.net', 'fbcdn.net']
   }
 };
