fe859fa51108a9378b58baa144c17566f4e5da93
ecosse3 <luk.kurpiewski@gmail.com>
2022-01-06 15:25:59 +0100

    feat: initialize google analytics in app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import '../styles/tailwind.css';
 import '../styles/bubbles.css';
+import { useEffect } from 'react';
+import ReactGA from 'react-ga';
 import type { AppProps } from 'next/app';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faEnvelope, faHome, faPhone } from '@fortawesome/free-solid-svg-icons';
@@ -10,6 +12,14 @@ import GlobalStyles from '../styles/GlobalStyles';
 library.add(faHome, faPhone, faEnvelope, faFacebook, faTwitter, faLinkedin, faSmile);
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    // Checks for GA ID and only turns on GA in production
+    if (process.env.NEXT_PUBLIC_ANALYTICS_ID && process.env.NODE_ENV === 'production') {
+      ReactGA.initialize(process.env.NEXT_PUBLIC_ANALYTICS_ID);
+      ReactGA.pageview(window.location.pathname + window.location.search);
+    }
+  });
+
   return (
     <div>
       <GlobalStyles />
