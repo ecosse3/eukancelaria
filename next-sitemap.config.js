546a9a5af7c242ef2ccff85803c9c2892b1bbd0d
ecosse3 <luk.kurpiewski@gmail.com>
2023-05-23 22:13:07 +0200

    chore: add next-sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.NEXT_PUBLIC_DOMAIN_URL || 'https://eukancelaria.pl',
+  generateRobotsTxt: true // (optional)
+};
